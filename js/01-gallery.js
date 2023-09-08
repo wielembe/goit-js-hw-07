6d41358614a7601af8c029edca4dc70986ee2c30
wielembe <e.wielemborek@gmail.com>
2023-09-08 18:18:06 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryElements = galleryItems
+    .map((picture) => {
+        return `<li><div class="gallery__item">
+     <a class="gallery__link" href="${picture.original}">
+       <img
+        class="gallery__image"
+         src="${picture.preview}"
+         data-source="${picture.original}"
+         alt="${picture.description}"
+       />
+     </a>
+   </li>`;
+    })
+    .join("");
+
+galleryList.innerHTML = galleryElements;
+
+galleryList.addEventListener("click", selectImage);
+
+function selectImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    const selectedImage = event.target.dataset.source;
+    const openedImage = basicLightbox.create(`<img src=` + selectedImage + `>`);
+    openedImage.show();
+
+    document.addEventListener("keydown", escapePress);
+    function escapePress(eventKey) {
+        if (eventKey.code === "Escape") {
+            openedImage.close();
+            document.removeEventListener("keydown", escapePress);
+        }
+    }
+}
