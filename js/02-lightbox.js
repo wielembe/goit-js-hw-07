9fd11c2c08495d3fdd49a82dbcf1a33448a4426b
wielembe <e.wielemborek@gmail.com>
2023-09-08 21:01:21 +0200

    correcions

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryElements = galleryItems
+    .map((picture) => {
+        return `<a class="gallery__item" href="${picture.original}">
+       <img
+        class="gallery__image"
+         src="${picture.preview}"
+
+         alt="${picture.description}"
+       />
+     </a>
+   `;
+    })
+    .join("");
+
+galleryList.innerHTML = galleryElements;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionSelector: "self",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
